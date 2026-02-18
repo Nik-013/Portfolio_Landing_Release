058ab3af05cc9a6780b1c13c708d5a954eee9d79
Nik-013 <lordrolap@gmail.com>
2026-02-18 14:34:29 +0400

    fix: Настроить модульный импорт CSS для исправления путей к ресурсам (v0.1.4-beta)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-// js/main.js
+import '../styles/base.css';
+import '../styles/header.css';
+import '../styles/about.css';
+import '../styles/skills.css';
+import '../styles/portfolio.css';
+import '../styles/footer.css';
+import '../styles/modal.css';
+
 import './scroll.js';
 import './menu.js';
 import './popup.js';
 import './status.js';
 import './protect.js';
-
